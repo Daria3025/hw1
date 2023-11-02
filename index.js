680feceaa181441152c659b7eb795dbc59b990cb
Daria3025 <dtimofeeva191@icloud.com>
2023-11-02 13:30:48 +0200

    hw1

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = "Daria";
+const admin = name;
+console.log(admin);
+
+const days = Math.ceil(Math.random() * 10);
+const seconds = `${days * 60 * 60 * 24} seconds`;
+console.log(seconds);
+
+const userAge = prompt("Please enter your age");
+console.log(userAge);
